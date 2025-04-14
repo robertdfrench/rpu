4a75d0d0d732e111e2dbdb3229c4845faa9c2723
Robert D. French <robert@robertdfrench.me>
2025-04-13 23:42:25 -0400

    Use native byte packing / unpacking

--- src/instructions.rs
+++ src/instructions.rs
@@ -49,60 +49,39 @@ impl Instruction {
     }
 
     fn to_u32(&self) -> u32 {
-        let b0 = 2_u32.pow(0);
-        let b1 = 2_u32.pow(8);
-        let b2 = 2_u32.pow(16);
-        let b3 = 2_u32.pow(24);
-
         match self {
             Instruction::hcf => 0,
             Instruction::put(val, dst) => {
-                let dst = dst.as_u8() as u32;
-                let val = *val as u32;
-                1 + val*b1 + dst*b3
+                let v = val.to_ne_bytes();
+                u32::from_ne_bytes([1,v[0],v[1],dst.as_u8()])
             },
             Instruction::add(src, dst) => {
-                let src = src.as_u8() as u32;
-                let dst = dst.as_u8() as u32;
-                2 + src*b1 + dst*b2
+                u32::from_ne_bytes([2,src.as_u8(),dst.as_u8(),0])
             },
             Instruction::cp(src, dst) => {
-                let src = src.as_u8() as u32;
-                let dst = dst.as_u8() as u32;
-                3 + src*b1 + dst*b2
+                u32::from_ne_bytes([3,src.as_u8(),dst.as_u8(),0])
             }
         }
     }
 
     fn try_from_u32(encoded: u32) -> Result<Self> {
-        let b0 = 2_u32.pow(0);
-        let b1 = 2_u32.pow(8);
-        let b2 = 2_u32.pow(16);
-        let b3 = 2_u32.pow(24);
-
-        let instr: u32 = encoded % b1;
-        let encoded: u32 = (encoded - instr)/b1;
+        let bytes = encoded.to_ne_bytes();
+        let instr = bytes[0];
         match instr {
             0 => Ok(Instruction::hcf),
             1 => {
-                let val: u32 = encoded % b2;
-                let dst: u32 = (encoded - val)/b2;
-                let val: u16 = val as u16;
-                let dst = Register::try_from_u8(dst as u8)?;
+                let val = u16::from_ne_bytes([bytes[1],bytes[2]]);
+                let dst = Register::try_from_u8(bytes[3])?;
                 Ok(Instruction::put(val, dst))
             },
             2 => {
-                let src: u32 = encoded % b1;
-                let dst: u32 = (encoded - src)/b1;
-                let src = Register::try_from_u8(src as u8)?;
-                let dst = Register::try_from_u8(dst as u8)?;
+                let src = Register::try_from_u8(bytes[1])?;
+                let dst = Register::try_from_u8(bytes[2])?;
                 Ok(Instruction::add(src, dst))
             },
             3 => {
-                let src: u32 = encoded % b1;
-                let dst: u32 = (encoded - src)/b1;
-                let src = Register::try_from_u8(src as u8)?;
-                let dst = Register::try_from_u8(dst as u8)?;
+                let src = Register::try_from_u8(bytes[1])?;
+                let dst = Register::try_from_u8(bytes[2])?;
                 Ok(Instruction::cp(src, dst))
             },
             _ => Err(DecodeError::NoSuchInstruction(instr as u8).into())
@@ -137,30 +116,30 @@ mod tests {
 
     #[test]
     fn encode_instructions() {
-        let gr0 = Register::gr0.as_u8() as u32;
-        let gr1 = Register::gr1.as_u8() as u32;
-        let gr2 = Register::gr2.as_u8() as u32;
-        let gr3 = Register::gr3.as_u8() as u32;
-        let out = Register::out.as_u8() as u32;
-
-        let b0 = 2_u32.pow(0);
-        let b1 = 2_u32.pow(8);
-        let b2 = 2_u32.pow(16);
-        let b3 = 2_u32.pow(24);
+        let gr0 = Register::gr0.as_u8();
+        let gr1 = Register::gr1.as_u8();
+        let gr2 = Register::gr2.as_u8();
+        let gr3 = Register::gr3.as_u8();
+        let out = Register::out.as_u8();
 
         let pairs = vec![
             (Instruction::hcf, 0 as u32),
             (
                 Instruction::put(7, Register::gr0),
-                (1 + 7*b1 + gr0*b3) as u32
+                u32::from_ne_bytes([
+                    1,
+                    7_u16.to_ne_bytes()[0],
+                    7_u16.to_ne_bytes()[1],
+                    gr0
+                ])
             ),
             (
                 Instruction::add(Register::gr2, Register::gr1),
-                (2 + gr2*b1 + gr1*b2) as u32
+                u32::from_ne_bytes([2,gr2,gr1,0])
             ),
             (
                 Instruction::cp(Register::gr3, Register::out),
-                (3 + gr3*b1 + out*b2) as u32
+                u32::from_ne_bytes([3,gr3,out,0])
             )
         ];
         for (instr, expected) in pairs {
@@ -171,30 +150,30 @@ mod tests {
 
     #[test]
     fn decode_instructions() {
-        let gr0 = Register::gr0.as_u8() as u32;
-        let gr1 = Register::gr1.as_u8() as u32;
-        let gr2 = Register::gr2.as_u8() as u32;
-        let gr3 = Register::gr3.as_u8() as u32;
-        let out = Register::out.as_u8() as u32;
-
-        let b0 = 2_u32.pow(0);
-        let b1 = 2_u32.pow(8);
-        let b2 = 2_u32.pow(16);
-        let b3 = 2_u32.pow(24);
+        let gr0 = Register::gr0.as_u8();
+        let gr1 = Register::gr1.as_u8();
+        let gr2 = Register::gr2.as_u8();
+        let gr3 = Register::gr3.as_u8();
+        let out = Register::out.as_u8();
 
         let pairs = vec![
             (Instruction::hcf, 0 as u32),
             (
                 Instruction::put(7, Register::gr0),
-                (1 + 7*b1 + gr0*b3) as u32
+                u32::from_ne_bytes([
+                    1,
+                    7_u16.to_ne_bytes()[0],
+                    7_u16.to_ne_bytes()[1],
+                    gr0
+                ])
             ),
             (
                 Instruction::add(Register::gr2, Register::gr1),
-                (2 + gr2*b1 + gr1*b2) as u32
+                u32::from_ne_bytes([2,gr2,gr1,0])
             ),
             (
                 Instruction::cp(Register::gr3, Register::out),
-                (3 + gr3*b1 + out*b2) as u32
+                u32::from_ne_bytes([3,gr3,out,0])
             )
         ];
         for (expected, encoded) in pairs {
